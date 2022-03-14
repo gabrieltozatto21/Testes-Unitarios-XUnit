1fe803bb82b84b42ea594fff7057795d96abf05c
gabrieltozatto21 <gabriel.tozatto@hotmail.com>
2022-03-14 19:36:43 -0300

    Novas funcionalidades de testes com XUnit

---
+++ teste-xunit/PatioTestes.cs
@@ -0,0 +1,68 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace teste_xunit
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.Proprietario = "Gabriel Tozatto";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Vermelho";
+            veiculo.Modelo = "HB20";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+            Assert.Equal("vermelho", veiculo.Cor.ToLower());
+
+        }
+    
+        //teste com um volume maior de dados
+        [Theory]
+        [InlineData("Gabriel Tozatto", "asd-1233", "HB20S", "Vermelho")]
+        [InlineData("Paulo Tozatto", "asd-9878", "CIVIC", "Preto")]
+        [InlineData("Charlene Tozatto", "asd-9299", "PULSE", "Azul")]
+        public void ValidaFaturamentoComVariosVeiculos(string nome, string placa, string modelo, string cor)
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.Proprietario = nome;
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            veiculo.Placa = placa;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+            Assert.Equal(cor.ToLower(), veiculo.Cor.ToLower());
+        }
+
+    }
+}
--- teste-xunit/VeiculoTestes.cs
+++ teste-xunit/VeiculoTestes.cs
@@ -1,3 +1,4 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
 using System;
 using Xunit;
@@ -6,21 +7,54 @@ namespace teste_xunit
 {
     public class VeiculoTestes
     {
-        [Fact]
+        [Fact(DisplayName ="Teste aceleração do veiculo")]
+        //agrupador de testes
+        [Trait("Funcionalidade", "Acelerar")]
         public void TesteVeiculoAcelerar()
         {
+            //Padrão AAA
+            //Arrange - prepara cenário
             var veiculo = new Veiculo();
+            //Act - metodo sobre teste
             veiculo.Acelerar(10);
+            //Assert - validãção dos resultados obtidos com os esperados
             Assert.Equal(100, veiculo.VelocidadeAtual);
 
         }
 
-        [Fact]
+        [Fact(DisplayName = "Teste frenagem do veiculo")]
+        //agrupador de testes
+        [Trait("Funcionalidade", "Frear")]
         public void TestaVeiculoFrear()
         {
+            //Arrange - prepara cenário
             var veiculo = new Veiculo();
+            //Act - metodo sobre teste
             veiculo.Frear(10);
+            //Assert - validãção dos resultados obtidos com os esperados
             Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            //Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+
+            //Act
+            veiculo.Tipo = TipoVeiculo.Motocicleta;
+            //Assert
+            Assert.Equal(TipoVeiculo.Motocicleta, veiculo.Tipo);
+        }
+
+        //skip ignorará o teste seguinte
+        [Fact(Skip = "Teste ainda não implementado!")]
+        public void ValidaProprietaio()
+        {
+
         }
     }
 }
