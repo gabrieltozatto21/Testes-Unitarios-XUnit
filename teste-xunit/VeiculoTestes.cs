322f147600952bd0f2b0e2c8e22003782831e8b8
gabrieltozatto21 <gabriel.tozatto@hotmail.com>
2022-03-14 11:00:34 -0300

    Adicionar arquivos de projeto.

---
+++ teste-xunit/VeiculoTestes.cs
@@ -0,0 +1,26 @@
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace teste_xunit
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TesteVeiculoAcelerar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Acelerar(10);
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
